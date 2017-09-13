b82f183eed315618ced32411437cd70109bea09a
Spyl <spyl94@gmail.com>
2017-09-13 15:47:33 +0200

    Add apm-beta support

--- plugins.d/atom.sh
+++ plugins.d/atom.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-echo "⚛️  Atom editor will be shiny when you'll be back from your coffee/tea break!"
-apm upgrade -c false
+echo " ️  Atom editor will be shiny when you'll be back from your coffee/tea br$
+if hash apm-beta 2>/dev/null; then
+        apm-beta upgrade -c false
+fi
+if hash apm 2>/dev/null; then
+        apm upgrade -c false
+fi
 echo ""
