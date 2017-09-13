800e4d1e66a639d83d79081c97b7c546182fce76
Spyl <spyl94@gmail.com>
2017-09-13 15:52:05 +0200

    Indent 8 -> 4

--- plugins.d/atom.sh
+++ plugins.d/atom.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "⚛️  Atom editor will be shiny when you'll be back from your coffee/tea break!"
 if hash apm-beta 2>/dev/null; then
-        apm-beta upgrade -c false
+    apm-beta upgrade -c false
 fi
 if hash apm 2>/dev/null; then
-        apm upgrade -c false
+    apm upgrade -c false
 fi
 echo ""
