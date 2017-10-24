2f67cf3921e0ab24821354400df9df23e49f9d81
Dan Chan <dchan@betterment.com>
2017-10-24 00:12:26 -0400

    Small hack to print === to width of terminal

--- kymsu.sh
+++ kymsu.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 echo "Please, grab a ☕️ , KYMSU keep your working environment up to date!"
-echo "=============================================================="
-echo ""
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
 
 [ -d ~/.kymsu/plugins.d -a -x ~/.kymsu/plugins.d ] && find ~/.kymsu/plugins.d -type f -name '*.sh' -perm +u+x -exec bash -c {} ${1:no-cleanup} \;
