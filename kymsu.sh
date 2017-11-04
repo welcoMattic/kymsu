8cbacf8304fbad0cec9e3ffaddb9401e699b099a
Dan Chan <dchan@betterment.com>
2017-11-04 17:48:15 -0400

    set max width to 50

--- kymsu.sh
+++ kymsu.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 echo "Please, grab a ☕️ , KYMSU keep your working environment up to date!"
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
+
+MAX_WIDTH=50
+CURRENT_WIDTH=$(tput cols)
+COLUMNS=$(( CURRENT_WIDTH > MAX_WIDTH ? MAX_WIDTH : CURRENT_WIDTH ))
+printf '%*s\n' "${COLUMNS}" '' | tr ' ' =
 
 [ -d ~/.kymsu/plugins.d -a -x ~/.kymsu/plugins.d ] && find ~/.kymsu/plugins.d -type f -name '*.sh' -perm +u+x -exec bash -c {} ${1:no-cleanup} \;
