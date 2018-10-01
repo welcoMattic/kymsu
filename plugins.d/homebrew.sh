f95a2cd610806acd763555799f978089d48bc549
Mathieu Santostefano <msantostefano@jolicode.com>
2018-10-01 22:21:32 +0200

    🐛  Check if brew is available

--- plugins.d/homebrew.sh
+++ plugins.d/homebrew.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
-echo "🍺  Homebrew"
-brew update
-brew upgrade
-brew cask outdated | cut -f 1 | xargs brew cask reinstall
-echo ""
+if hash brew 2>/dev/null; then
+  echo "🍺  Homebrew"
+  brew update
+  brew upgrade
+  brew cask outdated | cut -f 1 | xargs brew cask reinstall
+  echo ""
 
-echo "👨‍⚕  ️The Doc is checking that everything is ok."
-brew doctor
-brew missing
-echo ""
+  echo "👨‍⚕  ️The Doc is checking that everything is ok."
+  brew doctor
+  brew missing
+  echo ""
 
-if [[ $1 == "cleanup" ]]; then
-  echo "🌬  Cleaning brewery"
-  brew cleanup -s
-  brew cask cleanup
+  if [[ $1 == "cleanup" ]]; then
+    echo "🌬  Cleaning brewery"
+    brew cleanup -s
+    brew cask cleanup
+  fi
 fi
