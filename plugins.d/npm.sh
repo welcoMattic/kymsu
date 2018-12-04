6df5c76b748723ff0ea98d8f46fb8a5bd08fbc9e
Hans Dushanthakumar <hans.dushanthakumar@amaysim.com.au>
2018-12-04 16:14:43 +1100

    Clean npm cache only if npm version less than 5.0.0

--- plugins.d/npm.sh
+++ plugins.d/npm.sh
@@ -12,8 +12,16 @@ if hash npm 2>/dev/null; then
     npm update -g
     echo ""
 
-  if [[ $1 == "cleanup" ]]; then
-    echo "🌬  Cleaning npm cache"
-    npm cache clean
-  fi
+    if [[ $1 == "cleanup" ]]; then
+      if printf '%s\n%s\n' "$(npm --version)" 5.0.0 | sort --version-sort --check=silent; then
+        echo "🌬   Cleaning npm cache"
+        npm cache clean
+      fi
+    fi
+
+    echo "🌬   Verifying npm cache"
+    npm cache verify
+
+    echo "🌬   Running npm health check"
+    npm doctor
 fi
